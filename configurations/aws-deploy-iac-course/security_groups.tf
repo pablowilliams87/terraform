a4a8d12450fd98839209bb1f885b745242146ed5
unknown <pablo.williams87@gmail.com>
2020-09-26 13:07:27 -0300

    add SG

---
+++ configurations/aws-deploy-iac-course/security_groups.tf
@@ -0,0 +1,88 @@
+resource "aws_security_group" "lb-sg" {
+  provider      = aws.region-master
+  name          = "lb-sg"
+  description   = "Allow 443 and traffic to Jenkins SG"
+  vpc_id        = aws_vpc.vpc_master.id
+  ingress {
+    description = "Allows 443 from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allows 80 from anywhere for redirection"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "jenkins-sg" {
+  provider      = aws.region-master
+  name          = "jenkins-sg"
+  description   = "Allow TCP/8080 and TCP/22"
+  vpc_id        = aws_vpc.vpc_master.id
+  ingress {
+    description = "Allows 22 from our public IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["var.external_ip"]
+  }
+  ingress {
+    description = "Allows anyone on port 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["aws_security_group.lb-sg.id"]
+  }
+  ingress {
+    description = "Allows traffic from us-west-2"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["192.168.1.0/24"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "jenkins-sg-oregon" {
+  provider      = aws.region-worker
+  name          = "jenkins-sg-oregon"
+  description   = "Allow TCP/22"
+  vpc_id        = aws_vpc.vpc_master_oregon.id
+  ingress {
+    description = "Allows 22 from our public IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["var.external_ip"]
+  }
+  ingress {
+    description = "Allows traffic from us-east-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
