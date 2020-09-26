b0eaa329c2fc69281691b18d79ed027d80d20287
Pablo Williams <pablo.williams87@gmail.com>
2020-09-26 02:25:15 -0300

    Add network configuration

--- configurations/aws-s3-backend/example.tf
+++ configurations/aws-s3-backend/example.tf
@@ -1,11 +1,9 @@
-terraform
-{
+terraform {
   required_version = ">=0.12.0"
-  backend "s3"
-  {
-    region = "us-east-1"
+  backend "s3" {
+    region  = "us-east-1"
     profile = "default"
-    key = "<state-file-name>"
-    bucket = "<name-of-already-created-bucket>"
+    key     = "terra-backed-statefile"
+    bucket  = "terratest-pw-backend"
   }
 }
