193afd66a266e416f80f7b05e2f4faf406bc5298
unknown <pablo.williams87@gmail.com>
2020-10-04 23:31:17 -0300

    add ec2-simple-instance

---
+++ configurations/aws-simple-ec2/aws-simple-ec2.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-0947d2ba12ee1ff75"
+    instance_type = "t3.micro"
+}
+
