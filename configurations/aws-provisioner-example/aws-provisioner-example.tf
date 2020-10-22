9bd89e5b922a90ef107ba78cbcc1b8ac775a9564
unknown <pablo.williams87@gmail.com>
2020-10-22 09:37:52 -0300

    Modify provisioner example

--- configurations/aws-provisioner-example/aws-provisioner-example.tf
+++ configurations/aws-provisioner-example/aws-provisioner-example.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "myec2" {
   ami           = "ami-0947d2ba12ee1ff75"
   instance_type = "t2.micro"
-  # key           = terraform
+  key_name      = "terraform"
   provisioner "remote-exec" {
     inline = [
       "sudo amazon-linux-extras install -y nginx1.12",
@@ -16,13 +16,31 @@ resource "aws_instance" "myec2" {
     type        = "ssh"
     host        = self.public_ip
     user        = "ec2-user"
-    private_key = "${file("./terraform")}"
+    private_key = "file(./terraform)"
   }
 }
 
+
 resource "aws_instance" "web" {
-  # ...
+  ami           = "ami-0947d2ba12ee1ff75"
+  instance_type = "t2.micro"
   provisioner "local-exec" {
     command = "echo ${aws_instance.web.private_ip} >> private_ips.txt"
+  }
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0947d2ba12ee1ff75"
+  instance_type = "t2.micro"
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo 'Destroy-time provisioner'"
+  }
+  provisioner "remote-exec" {
+    when = destroy
+    inline = [
+      "sudo yum -y remove nano"
+    ]  
   }
 }
